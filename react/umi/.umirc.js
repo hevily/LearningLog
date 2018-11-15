977921b67881fcfe474ea850d0cdfce410858d8d
tangyunxiao <tangyx@lybrc.com.cn>
2018-11-15 11:00:53 +0800

    test: 测试umi配置

--- react/umi/.umirc.js
+++ react/umi/.umirc.js
@@ -18,4 +18,13 @@ export default {
       },
     ],
   ],
+  // base: '/dist/',   // 当路由不为hash的时候项目部署到非根目录，需要添加此配置
+  // publicPath: '/dist/', // 项目部署到非根目录需添加
+  history: 'hash', // 启用hash路由
+  hash: true, // 打包文件名字带有hash字符串
+  exportStatic: {
+    // exportStatic 按路由生成文件夹，文件夹内为此路由下的入口html文件
+    dynamicRoot: true, // 请求静态文件的路径设置为相对路径,存在问题：css引入的图片途径没有加入公共path
+    htmlSuffix: true, // 设置后将路由直接生成html文件，而不是放在文件夹内
+  },
 };
--- react/umi/src/pages/index.js
+++ react/umi/src/pages/index.js
@@ -1,13 +1,40 @@
+import { routerRedux } from 'dva/router';
+import React from 'react';
+import { connect } from 'dva';
 import styles from './index.css';
-
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
-      </ul>
-    </div>
-  );
+console.log('routerRedux---', routerRedux);
+@connect(state => {
+  console.log('state---', state);
+  return state;
+})
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <div className={styles.normal}>
+        <div className={styles.welcome} />
+        <ul className={styles.list}>
+          <li>
+            To get started, edit <code>src/pages/index.js</code> and save to reload.
+          </li>
+          <li>
+            <a href="https://umijs.org/guide/getting-started.html">Getting Started</a>
+          </li>
+          <li
+            onClick={() => {
+              console.log('点击跳转', this);
+              this.props.dispatch(
+                routerRedux.push({
+                  pathname: '/lazy',
+                })
+              );
+            }}
+          >
+            TO Lazy
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
+
+export default IndexPage;
