7d73c895faadc82779b004abda026bf21407ecd1
tangyunxiao <tangyx@lybrc.com.cn>
2018-12-12 11:29:48 +0800

    :sparkles: 添加深拷贝方法

--- js-methods/util.js
+++ js-methods/util.js
@@ -509,3 +509,21 @@ function customLog(message, color = "black") {
 
   console.log(`%c${message}`, `color:${color}`);
 }
+
+/*
+* 深拷贝对象
+*/
+function deepCopy(source, target = {}) {
+  var key;
+  for (key in source) {
+      if (source.hasOwnProperty(key)) {                         // 意思就是__proto__上面的属性,我不拷贝
+          if (typeof(source[key]) === "object") {               // 如果这一项是object类型,就递归调用deepCopy
+              target[key] = Array.isArray(source[key]) ? [] : {};
+              deepCopy(source[key], target[key]);
+          } else {                                            // 如果不是object类型,就直接赋值拷贝
+              target[key] = source[key];
+          }
+      }
+  }
+  return target;
+}
